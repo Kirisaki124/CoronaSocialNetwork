c4efa79151bd0c82739bbb49d4bf9d185c808a66
Kirisaki <kirisaki124@yahoo.com>
2020-03-19 11:49:35 +0700

    Init: Analyzing and realtime database

--- app/src/main/java/hava/coronasocialnetwork/MainActivity.kt
+++ app/src/main/java/hava/coronasocialnetwork/MainActivity.kt
@@ -3,13 +3,16 @@ package hava.coronasocialnetwork
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
+    private var mAuth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        mAuth = FirebaseAuth.getInstance()
     }
 }
