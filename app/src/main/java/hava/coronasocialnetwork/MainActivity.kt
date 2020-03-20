b72c36f4724c209005fd3206584498f63e5b23bc
Kirisaki <kirisaki124@yahoo.com>
2020-03-20 12:14:35 +0700

    Refactor: DaoAuthen/ Add: User model

--- app/src/main/java/hava/coronasocialnetwork/MainActivity.kt
+++ app/src/main/java/hava/coronasocialnetwork/MainActivity.kt
@@ -10,7 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Dao.setContext(this)
-        DaoManagement().register("tungdshw4130134@fpt.edu.vn", "123@123a")
+//        DaoManagement().register("tu1ngd123sdfsdfqwedshsdw4130134@fpt.edu.vn", "123@123a", "0123123", "tung", "wuhan")
+        var userId: String? =
+            DaoManagement().login("tu1ngd123sdfsdfqwedshsdw4130134@fpt.edu.vn", "123@123a")
+
     }
 
 }
