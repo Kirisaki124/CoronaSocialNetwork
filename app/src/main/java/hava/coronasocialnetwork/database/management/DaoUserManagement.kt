3557d4717ba9f83d6653763fefb4a1f8d460d18c
Kirisaki <kirisaki124@yahoo.com>
2020-03-23 12:22:29 +0700

    Fix: Status

--- app/src/main/java/hava/coronasocialnetwork/database/management/DaoUserManagement.kt
+++ app/src/main/java/hava/coronasocialnetwork/database/management/DaoUserManagement.kt
@@ -4,20 +4,14 @@ import hava.coronasocialnetwork.database.operator.DaoUser
 import hava.coronasocialnetwork.model.User
 
 object DaoUserManagement {
-    // Change user's username using user id
-    // return true if success
     fun changeName(uid: String, name: String): Boolean {
         return DaoUser.changeUsername(uid, name)
     }
 
-    // Change user's address using user id
-    // return true if success
     fun changeAddress(uid: String, address: String): Boolean {
         return DaoUser.changeAddress(uid, address)
     }
 
-    // Change user's phone using user id
-    // return true if success
     fun changePhone(uid: String, phone: String): Boolean {
         return DaoUser.changePhone(uid, phone)
     }
