30096df1a70e4a399a2d212dd05633e33ee0d7c9
stalk <stalke4rhunter@gmail.com>
2020-03-24 10:12:18 +0700

    Cretae Post

--- app/src/main/java/hava/coronasocialnetwork/MainActivity.kt
+++ app/src/main/java/hava/coronasocialnetwork/MainActivity.kt
@@ -1,7 +1,9 @@
 package hava.coronasocialnetwork
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import hava.coronasocialnetwork.activities.CreatePostActivity
 import hava.coronasocialnetwork.database.management.DaoAuthenManagement
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -9,6 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCreate.setOnClickListener {
+            val intent = Intent(this, CreatePostActivity::class.java)
+            startActivity(intent)
+        }
 
         signOutButton.setOnClickListener {
             DaoAuthenManagement.signOut()
